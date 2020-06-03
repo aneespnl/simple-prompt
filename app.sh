d1fdadcaffc39253b40e15bf5ff5b19f6a80ad27
aneespnl <42742719+aneespnl@users.noreply.github.com>
2020-06-03 08:53:40 +0400

    create app.sh

---
+++ app.sh
@@ -0,0 +1,7 @@
+echo "# simple-prompt" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/aneespnl/simple-prompt.git
+git push -u origin master
+                
